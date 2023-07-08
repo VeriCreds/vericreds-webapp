96ed5d5c0ecac24af44dddf659e6b23453388ab3
Jeremy Gleason <jeremygleason@icloud.com>
2023-07-07 20:10:21 -0400

    Add basic modal component

---
+++ components/modals/Modal.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+const Modal = (props) => {
+  return (
+    <div className="modal-background">
+      <div className="modal-content">
+        Modal
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
