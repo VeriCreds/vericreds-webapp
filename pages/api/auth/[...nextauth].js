d5b393bd3e743f160926d9e3aababf060461130a
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2023-08-24 18:04:46 -0400

    add backend token to fontend

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import { MoralisNextAuthProvider } from "@moralisweb3/next";
+import axios from "axios";
 
 export default NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
@@ -9,11 +10,55 @@ export default NextAuth({
     async jwt({ token, user}) {
       if (user) {
         token.user = user;
+        // check if user is new, if they will need to add to database
+
+        console.log("token", token);
+        console.log("user.address", user.address);
+
+        const metamask_address = user.address;
+
+        const connectBackend = async () => {
+          await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/users/login`, {
+            password: process.env.NEXT_PUBLIC_USER_PASSWORD,
+            metamask_address: metamask_address
+          }, {
+            withCredentials: true
+          }).then((response) => {
+            console.log(response.data.data.token);
+            return response.data.data.token;
+          }).catch((err) => console.error(err))
+        }
+
+        try {
+          const response = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/users/${metamask_address}`);
+
+          if (!response.data.data) {
+            await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/users`, {
+              first_name: "",
+              last_name: "",
+              metamask_address: metamask_address,
+              password: process.env.NEXT_PUBLIC_USER_PASSWORD,
+              email_address: ""
+            });
+
+            const backendToken = await connectBackend();
+            if (backendToken) {
+              token.accessToken = backendToken;
+            }
+
+            connectBackend();
+          } else {
+            connectBackend();
+          }
+        } catch (e) {
+          console.error(e);
+        }
+        return token;
       }
-      return token;
     },
     async session({ session, token }) {
       session.user = token.user;
+      session.backendToken = token.accessToken;
       return session;
     },
   },
